e31c2df5aeac78e55e6f95df4b1174b21ff25434
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2023-09-29 12:23:39 -0500

    Attend Patrick's comments

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires="sqlfluff>=0.4.0",
+    install_requires="sqlfluff>=1.3.0",
     entry_points={"sqlfluff": [f"sqlfluff_{PLUGIN_LOGICAL_NAME} = {PLUGIN_ROOT_MODULE}"]},
 )
